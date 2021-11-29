1018c47a2655e1b95209a2395dfa6a9214f743a1
Sohee <sohee3480@gmail.com>
2021-11-29 22:31:54 +0900

    위치 검색 기능 추가

---
+++ app/src/main/java/com/example/finedust/presentation/location/LocationActivity.kt
@@ -0,0 +1,83 @@
+package com.example.finedust.presentation.location
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.finedust.R
+import com.example.finedust.data.DetailAddress
+import com.example.finedust.databinding.ActivityLocationBinding
+
+class LocationActivity : AppCompatActivity() {
+    private val viewModel: LocationViewModel by viewModels()
+    lateinit var binding: ActivityLocationBinding
+
+    val adapter: LocationAdapter by lazy {
+        LocationAdapter {
+            mainIntent(it)
+        }
+    }
+    private var searchText = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_location)
+        binding.locationRecyclerview.adapter = adapter
+        binding.locationActivity = this
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        val layoutManager = LinearLayoutManager(this)
+
+        binding.locationRecyclerview.layoutManager = layoutManager
+
+        binding.locationSearch.doOnTextChanged { text, _, _, _ ->
+            searchText = text.toString()
+        }
+        binding.searchImg.setOnClickListener {
+
+            if (searchText.isBlank()) {
+                Toast.makeText(this, "주소를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                searchLocation()
+            }
+
+            val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(binding.locationSearch.windowToken, 0)
+        }
+
+        viewModel.detailAddressList.observe(this) {
+            adapter.addLocationList(it)
+            Log.d("주소 검색", "$it")
+
+        }
+        viewModel.detailAddressListNull.observe(this) {
+            Toast.makeText(this, "주소를 불러오지 못했습니다. 다시 입력해주시기 바랍니다.", Toast.LENGTH_SHORT).show()
+        }
+        viewModel.searchText.observe(this) {
+            Log.d("검색", it)
+        }
+    }
+
+    fun searchLocation() {
+
+        Log.d("주소 검색어", searchText)
+        viewModel.location(searchText)
+    }
+
+    fun mainIntent(data: DetailAddress) {
+        val result = Intent().apply {
+            putExtra("searchLocation", data)
+        }
+        setResult(RESULT_OK, result)
+        if (!isFinishing) finish()
+    }
+}
---
+++ app/src/main/java/com/example/finedust/presentation/location/LocationViewModel.kt
@@ -0,0 +1,57 @@
+package com.example.finedust.presentation.location
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.finedust.data.DetailAddress
+import com.example.finedust.data.repository.MainRepository
+import com.example.finedust.data.repository.MainRepositoryImpl
+import com.example.finedust.data.response.search.SearchAddressResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class LocationViewModel() : ViewModel() {
+
+    private val repository: MainRepository = MainRepositoryImpl()
+
+    private val _detailAddressList: MutableLiveData<List<DetailAddress>> = MutableLiveData()
+    val detailAddressList: LiveData<List<DetailAddress>>
+        get() = _detailAddressList
+
+    private val _detailAddressListNull: MutableLiveData<Unit> = MutableLiveData()
+    val detailAddressListNull: LiveData<Unit>
+        get() = _detailAddressListNull
+
+    private val _searchText: MutableLiveData<String> = MutableLiveData("")
+    val searchText: LiveData<String>
+        get() = _searchText
+
+    fun location(query: String) {
+        val callback = object : Callback<SearchAddressResponse> {
+            override fun onResponse(
+                call: Call<SearchAddressResponse>,
+                response: Response<SearchAddressResponse>
+            ) {
+
+                Log.d("위치 검색", response.toString())
+                Log.d("검색어", query)
+
+                val documentsList = response.body()!!.documents
+                val addressList = DetailAddress.convertDetailAddress(documentsList)
+
+                if (documentsList.isEmpty()) {
+                    _detailAddressListNull.value = Unit
+                } else {
+                    _detailAddressList.value = addressList
+                }
+            }
+
+            override fun onFailure(call: Call<SearchAddressResponse>, t: Throwable) {
+                Log.e("airConditionResponse", "에러 발생")
+            }
+        }
+        repository.getSearchLocation(query, callback)
+    }
+}
