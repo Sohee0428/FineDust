7030c75fdcc976e98d10b5c563f26819f64d5121
Sohee <sohee3480@gmail.com>
2021-12-06 19:29:43 +0900

    바인딩 어댑터 수정

--- app/src/main/java/com/example/finedust/presentation/BindingAdapter.kt
+++ app/src/main/java/com/example/finedust/presentation/BindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.finedust.presentation
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.finedust.App
@@ -16,6 +17,42 @@ object BindingAdapter {
             "3" -> view.text = "나쁨"
             "4" -> view.text = "매우나쁨"
             else -> view.text = "오류"
+        }
+    }
+
+    @BindingAdapter("setImageForCAIGrade")
+    @JvmStatic
+    fun setImageForCAIGrade(view: ImageView, grade: String?) {
+        when (grade) {
+            "1" -> view.setImageResource(R.drawable.perfect_bearflower)
+            "2" -> view.setImageResource(R.drawable.good_bearflower)
+            "3" -> view.setImageResource(R.drawable.soso_bearflower)
+            "4" -> view.setImageResource(R.drawable.bad_bearflower)
+            else -> view.setImageResource(R.drawable.ic_baseline_error_24)
+        }
+    }
+
+    @BindingAdapter("setImageForPM10Grade")
+    @JvmStatic
+    fun setImageForPM10Grade(view: ImageView, value: String?) {
+        when (value?.toIntOrNull()) {
+            in 0..30 -> view.setImageResource(R.drawable.perfect_bearflower)
+            in 31..50 -> view.setImageResource(R.drawable.good_bearflower)
+            in 51..100 -> view.setImageResource(R.drawable.soso_bearflower)
+            null -> view.setImageResource(R.drawable.loading)
+            else -> view.setImageResource(R.drawable.bad_bearflower)
+        }
+    }
+
+    @BindingAdapter("setImageForPM25Grade")
+    @JvmStatic
+    fun setImageForPM25Grade(view: ImageView, value: String?) {
+        when (value?.toIntOrNull()) {
+            in 0..15 -> view.setImageResource(R.drawable.perfect_bearflower)
+            in 16..25 -> view.setImageResource(R.drawable.good_bearflower)
+            in 26..50 -> view.setImageResource(R.drawable.soso_bearflower)
+            null -> view.setImageResource(R.drawable.loading)
+            else -> view.setImageResource(R.drawable.bad_bearflower)
         }
     }
 
