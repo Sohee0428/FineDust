b84642fbb3d452ee09c872e56ae01e29b7c6f002
Sohee0428 <82200065+Sohee0428@users.noreply.github.com>
2022-03-24 15:42:56 +0900

    log 확장함수 작성

--- app/src/main/java/com/sohee/finedust/Extensions.kt
+++ app/src/main/java/com/sohee/finedust/Extensions.kt
@@ -1,8 +1,17 @@
 package com.sohee.finedust
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 
 fun Context.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun logHelper(title: String, content: String) {
+    Log.d(title, content)
+}
+
+fun logHelper(content: String) {
+    Log.d("LogHelper", content)
 }
