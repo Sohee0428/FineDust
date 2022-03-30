9c95117700fdadf2744440dbcfa82602a411837c
Sohee0428 <82200065+Sohee0428@users.noreply.github.com>
2022-03-30 22:52:13 +0900

    InAppUpdate 적용  - 적용만 한 상태

--- app/src/test/java/com/sohee/finedust/ExampleUnitKakaoResponse.kt
+++ app/src/test/java/com/sohee/finedust/ExampleUnitKakaoResponse.kt
@@ -1,6 +1,14 @@
 package com.sohee.finedust
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import org.junit.Test
+import com.sohee.finedust.App
 
 import org.junit.Assert.*
 
@@ -10,8 +18,24 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitKakaoResponse {
+//    val fusedLocationClient: FusedLocationProviderClient
+//            = LocationServices.getFusedLocationProviderClient(App().context)
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+//        if (ActivityCompat.checkSelfPermission(
+//                App().context,
+//                Manifest.permission.ACCESS_FINE_LOCATION
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+//                if (location != null) {
+//                    val latitude = location.latitude
+//                    val longitude = location.longitude
+//                    Log.d("Test", "GPS Location Latitude: $latitude" +
+//                            ", Longitude: $longitude")
+//                }
+//            }
+//        }
     }
 }
