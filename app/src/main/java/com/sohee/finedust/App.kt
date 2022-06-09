0b08b47d515fa6db7000cb841a06f1d80c101034
Sohee0428 <82200065+Sohee0428@users.noreply.github.com>
2022-06-09 19:25:49 +0900

    Hilt 기본 설정  - HiltAndroidApp

--- app/src/main/java/com/sohee/finedust/App.kt
+++ app/src/main/java/com/sohee/finedust/App.kt
@@ -1,8 +1,10 @@
-package com.sohee.finedust.ui
+package com.sohee.finedust
 
 import android.app.Application
 import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
